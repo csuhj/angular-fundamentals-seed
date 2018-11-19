6c7603efb05d350a0a62718751c7ee4e4d167371
Adam Robertson <adam.robertson@apteco.com>
2018-11-19 15:43:45 +0000

    created a passenger count component

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Passenger } from '../models/passenger.interface';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
     selector: 'passenger-dashboard',
     styleUrls: ['passenger-dashboard.component.scss'],
     template: `
         <div>
+            <passenger-count
+              [items]="passengers">
+            </passenger-count>
+            <passenger-detail>
+            </passenger-detail>
             <h3>Airline Passengers</h3>
             <ul>
             <li *ngFor="let passenger of passengers; let i = index;">
