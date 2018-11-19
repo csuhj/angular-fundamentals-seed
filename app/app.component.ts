81a4bac29947f67f0d070ff390e35195d808762e
Adam Robertson <adam.robertson@apteco.com>
2018-11-19 13:15:46 +0000

    interpolation expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   templateUrl: `
     <div class="app">
-      {{ title }}
+      {{ title + '!' }}
+      <div>
+        {{ numberOne + numberTwo }}
+      </div>
+      <div>
+        {{ isHappy ? ':)' : ':(' }}
+      </div>
     </div>
   `
 })
 export class AppComponent {
   title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = false;
 
   constructor() {
     this.title = 'Ultimate Angular'
