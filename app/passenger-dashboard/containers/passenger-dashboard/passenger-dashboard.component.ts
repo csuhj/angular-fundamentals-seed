624e1800cae53eed6f47bde96741a875d73e3ef2
Adam Robertson <adam.robertson@apteco.com>
2018-11-19 15:50:33 +0000

    created the passenger-detail component

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -10,23 +10,11 @@ import { Passenger } from '../../models/passenger.interface';
             <passenger-count
               [items]="passengers">
             </passenger-count>
-            <passenger-detail>
-            </passenger-detail>
             <ul>
-            <li *ngFor="let passenger of passengers; let i = index;">
-                <span 
-                class="status"
-                [class.checked-in]="passenger.checkedIn"
-                ></span>
-                {{ i }}: {{ passenger.fullname }}
-                <div class="date">
-                Check in date:
-                {{ passenger.checkInDate? (passenger.checkInDate | date: 'yMMMMd' | uppercase ) : 'Not checked in' }}
-                </div>
-                <div class="children">
-                Children: {{ passenger.children?.length || 0}}
-                </div>
-            </li>
+                <passenger-detail
+                    *ngFor="let passenger of passengers;"
+                    [detail]="passenger">
+                </passenger-detail>
             </ul>
         </div>
     `
