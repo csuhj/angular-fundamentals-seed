fac55c3d4401c28c6fd65afae256e711bd6b6458
Adam Robertson <adam.robertson@apteco.com>
2018-11-19 15:25:26 +0000

    fixed spelling mistakes

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PassengerDashboardComponent } from './containers/passenger-dashbord/passenger-dashbord.component';
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 
 @NgModule({
   declarations: [
