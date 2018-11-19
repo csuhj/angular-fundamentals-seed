85ca36f31dcd5efb3f56fc2446209f74e0b6a525
Adam Robertson <adam.robertson@apteco.com>
2018-11-19 15:24:35 +0000

    created a custom feature module

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PassengerDashboardComponent } from './containers/passenger-dashbord/passenger-dashbord.component';
+
 @NgModule({
   declarations: [
+    PassengerDashboardComponent
   ],
   imports: [
     CommonModule,
+  ],
+  exports: [
+    PassengerDashboardComponent
   ]
 })
 export class PassengerDashboardModule {}
